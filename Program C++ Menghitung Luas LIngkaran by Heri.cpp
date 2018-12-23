e4bc2b5dbb9960006949b03650e2297eec71ec7d
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-23 20:29:03 +0700

    Add files via upload

---
+++ Program C++ Menghitung Luas LIngkaran by Heri.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    float phi=3.14,luas;
+
+cout<<"PROGRAM MENGHITUNG LUAS LINGKARAN"<<endl;
+
+cout<<"Masukan jari-jari lingkaran = ";
+cin>>r;
+luas=phi*r*r;
+cout<<"Luas Lingkaran Adalah       = "<<luas;
+return 0;
+}
